93d24925dcd732fb763740f9bc1a96ab1cd4e98a
nehsa25 <jesse.stone@nehsa.net>
2024-06-04 07:54:54 -0700

    Added test file

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/**
+ * This is a simple Java program.
+ */
+
+ public class Hello {
+     // Your program begins with a call to main().
+     // Prints "Hello, World" to the terminal window.
+     public static void main(String[] args) {
+        System.out.println("Double");
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println("Float:");
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+     }
+ }
